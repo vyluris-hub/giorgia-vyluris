5e9bb869e4fbe93baae42f4ad5893958de9d9062
vyluris-hub <vyluris@gmail.com>
2026-05-31 10:20:51 +0200

    Update index-openai-realtime-vyluris.js

--- index-openai-realtime-vyluris.js
+++ index-openai-realtime-vyluris.js
@@ -220,7 +220,7 @@ wss.on('connection', (twilioWs) => {
     openaiWs = new WebSocket('wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview', {
       headers: {
         Authorization: `Bearer ${OPENAI_API_KEY}`,
-        'OpenAI-Beta': 'realtime=v1'
+        
       }
     });
 
